a5fa7fa6a8b8d89c80f48a69ffa174b19c9eea50
SulistiawanSatria <satrialimpad2@gmail.com>
2026-07-15 17:35:36 +0700

    error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
@@ -8,6 +9,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lumovelo.agency',
+  adapter: vercel(),
   integrations: [sitemap(), react()],
   i18n: {
     defaultLocale: 'id',
